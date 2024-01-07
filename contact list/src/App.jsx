d1c365ef35c8a3462d5e26f91f181bb9bc7d7eda
betheljn <betheljn@jairusdesktop>
2024-01-06 19:19:15 -0500

    finished workshop

--- contact list/src/App.jsx
+++ contact list/src/App.jsx
@@ -1,15 +1,22 @@
-import { useState } from 'react'
+import ContactList from "./components/ContactList";
+import "./App.css";
+import {useState} from "react"
+// import ContactRow from "./components/ContactRow";
+import SelectedContact from "./components/SelectedContact";
 
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
+  const [selectedContactId, setSelectedContactId] = useState(null)
   return (
-    <>
-     
-    </>
-  )
-}
+    <> 
+    {selectedContactId ? (
+      <SelectedContact
+      selectedContactId={selectedContactId}
+      setSelectedContactId={setSelectedContactId}
+      />
+    ) : (
+      <ContactList setSelectedContactId={setSelectedContactId} />
 
-export default App
+    )}
+    </>
+  );
+}
